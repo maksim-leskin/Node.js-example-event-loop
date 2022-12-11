75ae8df41c872419c2a758906a2a528ed8a31aee
Maksim Leskin <quper24@mail.ru>
2022-12-11 23:30:49 +0300

    change Letter

--- index_1.js
+++ index_1.js
@@ -1,29 +1,27 @@
 const fs = require('node:fs');
 
-console.log('A console.log', performance.now());
-
 fs.readFile('./text.txt', 'utf8', (err, data) => {
-    console.log('B readFile', performance.now());
+    console.log('A readFile', performance.now());
 });
 
 setTimeout(() => {
-  console.log('C setTimeout', performance.now());
+  console.log('B setTimeout', performance.now());
 });
 
 setTimeout(() => {
-  console.log('D setTimeout 100', performance.now());
+  console.log('C setTimeout 100', performance.now());
 }, 100);
 
 setTimeout(() => {
-  console.log('E setTimeout 0', performance.now());
+  console.log('D setTimeout 0', performance.now());
 }, 0);
 
 setImmediate(() => {
-  console.log('F setImmediate', performance.now());
+  console.log('E setImmediate', performance.now());
 });
 
 fs.writeFile('text.txt', 'Hello Node.js', 'utf8', () => {
-  console.log('G writeFile', performance.now());
+  console.log('F writeFile', performance.now());
 });
 
-console.log('H console.log', performance.now());
+console.log('G console.log', performance.now());
